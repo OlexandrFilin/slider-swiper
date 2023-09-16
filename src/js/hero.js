61ffe00139cd09d4a580e736d8eda90ba7fce7c5
OleksandrFilin <acfiln@ukr.net>
2023-09-16 18:27:16 +0300

    fix

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,33 +1,4 @@
-// import Swiper JS
-// import Swiper, { Navigation, Padination } from 'swiper';
-// // // import Swiper styles
-// //import 'swiper/css';
-
-// const swiper = new Swiper('.swiper-container', {
-//   //   // Optional parameters
-//   //direction: 'horizontal',
-//   direction: 'vertical',
-//   loop: true,
-//   spaceBetween: 10,
-
-//   //   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-
-//   // Navigation arrows
-//   //   navigation: {
-//   //     nextEl: '.swiper-button-next',
-//   //     prevEl: '.swiper-button-prev',
-//   //   },
-
-//   //   // And if we need scrollbar
-//   //   scrollbar: {
-//   //     el: '.swiper-scrollbar',
-//   // },
-// });
-// core version + navigation, pagination modules:
+console.log('swiper');
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
